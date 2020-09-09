2363c44a41dfc938d63acf5011d0c4f3cb4a48ec
Rafael Barbosa <lul.rafaelbarbosa@gmail.com>
2020-09-08 22:53:47 -0300

    Add files via upload

---
+++ 04API_Flask/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+import numpy as np
+from pickle import load
+
+app = Flask(__name__)
+model = load(open('model.pkl', 'rb'))
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Está funcionando essa desgraça</h1> <p> Resposta: Sim </p>"
+
+@app.route("/results", methods = ["POST"])
+
+def predict():
+    
+    r = request.form
+
+    data = [float(x) for x in r.values()]
+    data = np.array(data).reshape(1, -1)
+
+    prediction = model.predict(data)
+    prediction = str(prediction)
+    
+    return {'prediction': prediction}
+
+if __name__ == "__main__":
+    app.run(debug = True)
